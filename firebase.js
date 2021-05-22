29b9f2e269d842483ed2afdb73575c535b52d6b3
Goldie Kedia <namasteyoldie@gmail.com>
2021-05-22 20:31:09 +0530

    Initial setup for signal clone app

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkR3ptG0M5kEV-bahK7uJRM5Xxnmrc-AY",
+  authDomain: "signal-clone-yt-build-33ef8.firebaseapp.com",
+  projectId: "signal-clone-yt-build-33ef8",
+  storageBucket: "signal-clone-yt-build-33ef8.appspot.com",
+  messagingSenderId: "674506914088",
+  appId: "1:674506914088:web:08133ab97736f12d69f81d"
+};
---
+++ screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet,Text,View} from 'react-native';
+
+const LoginScreen=()=>{
+	return (
+		<View>
+		<Text>I am Login Screen</Text>
+		</View>
+	)
+
+}
+
+export default LoginScreen;
+
